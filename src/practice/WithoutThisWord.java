9c4b2934976a798e7c9aebc875607287346f70c5
suraj <surajkishorsk@gmail.com>
2024-07-04 16:21:10 +0530

    more code

--- src/practice/WithoutThisWord.java
+++ src/practice/WithoutThisWord.java
@@ -4,18 +4,18 @@ public class WithoutThisWord {
 	
 	int a ,b;
 	WithoutThisWord(int a, int b ){
-		this.a=a ;
-		this.b=b;
- 
+		a=a ;
+		b=b;
+		
 			
 	}
-//	public void  show () {
-//		System.out.println(a);
-//		System.out.println(b);
-//	}
+	public void  show () {
+		System.out.println(a);
+		System.out.println(b);
+	}
 	public static void main(String[] args) {
 
 	WithoutThisWord wt=new WithoutThisWord(10,20);
-//	wt.show();
+	wt.show();
 	}
 }
